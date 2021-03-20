b423888ba183ef8532eab36cbaf17811ebabd42d
abelzach <59882990+abelzach@users.noreply.github.com>
2021-03-20 21:20:55 +0530

    Final changes made to help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,17 @@ module.exports = {
 			commands_data.push(`**Name:** ${command.name}\n**Description:** ${command.description}\n**Usage:** ${command.usage}`);
 			message.author.send(commands_data.join(' '), { split: true });
 		}
-		
+		else{
+			commands_data.push("✨Good Day! I'm **The Bot of Avon** Currently, I support the following commands:\n✨");
+			commands.map((command, id) => {
+				commands_data.push(` ***${command.name}***  |`);
+			})
+			var lastitem = commands_data.pop().slice(0, -2);
+			commands_data.push(lastitem);
+			commands_data.push(`✨`);
+        		commands_data.push(`\n Use !help [command] to receive a private message about a given command.`);
+			message.channel.send(commands_data.join(' '), { split: true });
+		}
 	}
     
 }
