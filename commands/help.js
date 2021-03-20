c6befe1655e733dcefcee53e74d2f1943f47f52d
abelzach <59882990+abelzach@users.noreply.github.com>
2021-03-20 21:19:02 +0530

    Created help command
    js file

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'help',
+	aliases: [],
+	description: 'displays the set of commands and their usage',
+	usage: '!help [command]',
+	execute(message, args, client){
+
+		
+	}
+    
+}
