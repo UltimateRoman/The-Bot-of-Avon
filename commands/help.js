5b6db65e2ff4e41e29ad93b34c037464c57026a9
abelzach <59882990+abelzach@users.noreply.github.com>
2021-03-20 21:20:17 +0530

    Modifed help.js

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,15 @@ module.exports = {
 	usage: '!help [command]',
 	execute(message, args, client){
 
+		const commands_data = [];
+		const { commands } = message.client;
+
+		if(args.length > 0){
+			if(!commands.has(args[0])){ message.channel.send('Sorry, but that command does not exist.'); return; }
+			command = commands.get(args[0]);
+			commands_data.push(`**Name:** ${command.name}\n**Description:** ${command.description}\n**Usage:** ${command.usage}`);
+			message.author.send(commands_data.join(' '), { split: true });
+		}
 		
 	}
     
